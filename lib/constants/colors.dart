5ce6863bf8693834d6f94acd0bba013de9c45669
Lea Antony <tonimemangkeren76@gmail.com>
2025-03-27 01:02:45 +0700

    feat(colors): update primary color and enhance color palette

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   // Primary color
-  static const Color primary = Color(0xFFF6A230);
+  static const Color primary = Color(0xFFEDA033);
 
   // Secondary color
   static const Color secondary = Color(0xFF5966B1);
 
   // Color pallete
   static const Color buff = Color(0xFFF3D58D);
-  static const Color background = Color(0xFFFFFFFD);
-  static const Color backgroundOnboarding = Color(0xFFFDEFC0);
+  static const Color white = Color(0xFFFFFFFD);
+  static const Color porcelain = Color(0xFFFDEFC0);
+  static const Color offWhite = Color(0xFFFBF5E6);
+  static const Color cream = Color(0xFFEBDDC5);
 
   // Text color
   static const Color textGrey = Colors.blueGrey;
